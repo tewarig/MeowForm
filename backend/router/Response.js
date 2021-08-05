66318d1daa6aafcaff157ab9e8081de073d71bf2
Gaurav Tewari <gauravtewari111@gmail.com>
2021-08-05 18:44:32 +0530

    basic email sending done

--- backend/router/Response.js
+++ backend/router/Response.js
@@ -1,17 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
 
 
 router.post("/:email&:formName",async(req,res)=>{
    let email = req.params.email ;
    let formName = req.params.formName;
-   let data = req.body;
+   let temp = req.body;
+   temp =  JSON.stringify(temp);  
+   temp = temp.replace(/{/g," ");
+   temp = temp.replace(/}/g," ");
+   temp = temp.split(",");
+   let title = [];
+   let entry = [];
+   for(let i=0;i<temp.length;i++){
+       temp[i] = temp[i].replace(/"/g,"");
+       let mainArray = temp[i].split(":");
+        title.push(mainArray[0]);
+        entry.push(mainArray[1]);
+
+   }
+   //    temp = temp.split(":");
+   let mailBody = "";
+   for(let i=0;i<temp.length;i++){
+       mailBody += `<b>${title[i]}</b> : <p>${entry[i]} <p>  <br/>`
+   }
+    //  console.log(mailBody);
+   let data = JSON.stringify(req.body);
 
        try{
            console.log(`email: ${email} `);
            console.log(`formName: ${formName}`);
-          console.log(data);
+          let testAccount = await nodemailer.createTestAccount();
+          let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+              user: "", // generated ethereal user
+              pass: "", // generated ethereal password
+            },
+          });
+          let info = await transporter.sendMail({
+            from: '"Meow Forms 🐱 " <hello@gauravtewari.xyz>', // sender address
+            to: `${email}`, // list of receivers
+            subject: `New response in ${formName}`, // Subject line
+            text: `${mailBody}`, // plain text body
+            html: `${mailBody}`, // html body
+          });
+        
+          console.log("Message sent: %s", info.messageId);
+          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        
+          // Preview only available when sending through an Ethereal account
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
           res.status(200).send("done") ;
       }
       catch(error){
