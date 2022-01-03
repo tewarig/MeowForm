dfa030ea23630183ad18928ce03ba7433921c8ab
Gaurav <hello@gauravtewar.xyz>
2022-01-03 17:52:01 +0530

    feat: FAQ updated

--- src/data/faq.js
+++ src/data/faq.js
@@ -1,24 +1,21 @@
 export const FAQ_Data = [{
         'id': 1,
-        'question': 'I signed up for Hacktoberfest mid-October. Will pull requests that I created earlier in October count?',
-        'answer': `Yes, all pull requests created between Oct 1 and Oct 31 will count, regardless of when you register for Hacktoberfest. 
-        Pull requests created before Oct 1 but merged or marked as ready for review after do not count. 
-        Pull requests that are still in review after Oct 31 and meet the criteria will count towards your completion goal. `
+        'question': 'How you are able to keep Meow Form Completely free?',
+        'answer': `Currently MeowForm's  is costing me around $5 a month I am able to cover this cost.`
     },
     {
         'id': 2,
-        'question': 'How do I get stickers?',
-        'answer': 'Stickers are included with your T-shirt, once you completed the requirements to earn a prize.'
+        'question': 'What if i do if a not receive email responses ?',
+        'answer': ' Currenlty due to bugeting Meowform has a cap of 30,000 emails a month . so if this limit is cross you would not receive any of the email. but still you can check you dashboard for new responses'
     },
     {
         'id': 3,
-        'question': 'What is open source? ',
-        'answer': 'Open source refers to source code that is publicly accessible and allows anyone to inspect, modify or learn from it. Open source projects encourage collaboration and the freedom to use the software for any purpose you wish.'
+        'question': 'How can i support Meowforms ? ',
+        'answer': 'You can support it by contributing to meowform in github or by telling the whole world about Meowforms or by buying me a coffe or Ice Tea'
     },
     {
         'id': 4,
-        'question': 'What should I do if I contribute to a repository that is not participating in Hacktoberfest?',
-        'answer': `You can politely request the maintainer of the project you contributed to, to tag your contribution with the label **hacktoberfest-accepted** . Or, add the 'hacktoberfest' topic to the repository to opt the project into Hacktoberfest.
-        Any pull request with the **hacktoberfest-accepted** label, submitted to any public GitHub/GitLab repository, with or without the hacktoberfest topic, will be considered valid for Hacktoberfest.`
+        'question': 'What if you stop Mainting MeowForms or Paying for the Services ',
+        'answer': `Surely MeowForms will go offline but the Code is open sourced so you can deploy a the code youself if you are heavly deponded on Meowform`
     },
 ]
